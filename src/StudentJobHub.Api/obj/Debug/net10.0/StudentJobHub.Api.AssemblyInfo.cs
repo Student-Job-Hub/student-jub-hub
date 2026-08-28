54f1524cfae41650907401c4ae57d1fcc04bcbf0
Addo Michael Obiri <moaddo004@st.ug.edu.gh>
2026-08-28 00:51:08 +0000

    feat: modified the working files and folders

--- src/StudentJobHub.Api/obj/Debug/net10.0/StudentJobHub.Api.AssemblyInfo.cs
+++ src/StudentJobHub.Api/obj/Debug/net10.0/StudentJobHub.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentJobHub.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c671bdaf8b0a5dd60f5f16516bdb6487835bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3300ce1014a137ad588c7d39dbb1b69de88beae")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentJobHub.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentJobHub.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/StudentJobHub.Client/Program.cs
+++ src/StudentJobHub.Client/Program.cs
@@ -5,33 +5,45 @@ using StudentJobHub.Client;
 using StudentJobHub.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiBaseUrl = "http://localhost:5205/";
 
+// Authentication service
 builder.Services.AddScoped<AuthService>(sp =>
     new AuthService(
-        new HttpClient { BaseAddress = new Uri(apiBaseUrl) },
+        new HttpClient
+        {
+            BaseAddress = new Uri(apiBaseUrl)
+        },
         sp.GetRequiredService<Microsoft.JSInterop.IJSRuntime>()));
 
+// JWT handler
 builder.Services.AddScoped<JwtAuthorizationHandler>();
 
+// Authorized API client
 builder.Services.AddHttpClient("AuthorizedClient", client =>
 {
     client.BaseAddress = new Uri(apiBaseUrl);
 })
 .AddHttpMessageHandler<JwtAuthorizationHandler>();
 
+// API services
 builder.Services.AddScoped<JobApiService>(sp =>
-    new JobApiService(sp.GetRequiredService<IHttpClientFactory>().CreateClient("AuthorizedClient")));
+    new JobApiService(
+        sp.GetRequiredService<IHttpClientFactory>()
+            .CreateClient("AuthorizedClient")));
 
 builder.Services.AddScoped<ApplicationApiService>(sp =>
-    new ApplicationApiService(sp.GetRequiredService<IHttpClientFactory>().CreateClient("AuthorizedClient")));
+    new ApplicationApiService(
+        sp.GetRequiredService<IHttpClientFactory>()
+            .CreateClient("AuthorizedClient")));
 
 var host = builder.Build();
 
-// Restore token from localStorage before the app renders
+// Restore JWT from localStorage
 var authService = host.Services.GetRequiredService<AuthService>();
 await authService.InitializeAsync();
 
