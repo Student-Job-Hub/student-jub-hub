b5cfe8f60fa79a433f4b445c59d29c0715382bd2
Addo Michael Obiri <moaddo004@st.ug.edu.gh>
2026-09-22 19:55:26 +0000

    chore: generate build artifacts and add initial client layout and style assets

--- src/StudentJobHub.Client/obj/Debug/net10.0/StudentJobHub.Client.AssemblyInfo.cs
+++ src/StudentJobHub.Client/obj/Debug/net10.0/StudentJobHub.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentJobHub.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12320ff5b0c16442933369b1c6aa00a60d33543d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8911077add8fc0b1eaa7821052b85cf83905770b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentJobHub.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentJobHub.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
